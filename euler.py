9f7a9d23de487a16db0ef65d0e362acc35c14322
Roy8743 <57109928+Roy8743@users.noreply.github.com>
2020-02-21 21:02:27 -0600

    Update euler.py

--- euler.py
+++ euler.py
@@ -1,8 +1,28 @@
+def factorial(n):
+	if(n==0):
+		return 1
+	else:
+		return n*factorial(n-1)
+
+
 def e_cuadratica(n):
-    # Implemente esta función
-    return 0
+	i=0
+	resp=0
+	while(i<n):
+		resp+=(1/factorial(i))
+		i+=1
+	return resp
+#print(e_cuadratica(100))
 
 
 def e_lineal(n):
-    # Implemente esta función
-    return 0
+	fact=1
+	i=1
+	resp=0
+	while(i<=n):
+		fact=fact*i
+		resp+=(1/fact)
+		i+=1
+	return resp+1
+
+#print(e_lineal(100000))
